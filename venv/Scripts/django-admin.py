a543442aa1cc64ec3202a1e6897a347b11cd1489
Nimesha Buddhika <135071J@uom.lk>
2018-01-06 01:53:50 +0530

    init project

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Nimesha Buddhika\Documents\PycharmProject\Tensorflow_Chatbot\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
